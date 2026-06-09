980050bbe0de9699a6f35b83d7595b3e1176879e
hackerdarkapp-hash <hackerdarkapp@gmail.com>
2026-06-10 02:56:56 +0300

    fix: register camera and viewer screens in root Stack layout

--- artifacts/remote-cam/app/_layout.tsx
+++ artifacts/remote-cam/app/_layout.tsx
@@ -23,6 +23,8 @@ function RootLayoutNav() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+      <Stack.Screen name="camera" />
+      <Stack.Screen name="viewer" />
     </Stack>
   );
 }
